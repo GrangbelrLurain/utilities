edda50e1f172349522cf21c98b548b01844459f4
김규연 <lurain@withmode.com>
2024-12-24 14:30:00 +0900

    [feat] queryProperty
    - 쿼리 프로퍼티 더 많이 보이도록 개선

--- src/utils/text-format/query-format.tsx
+++ src/utils/text-format/query-format.tsx
@@ -1,23 +1,32 @@
-export type TUrlToJsonReturn = {
-  origin: string;
-  pathname: string;
-  searchParams?: { [key: string]: string };
-} | null;
+type TURLObj = {
+  [key: string]: string | TURLObj | undefined;
+};
+
+export type TUrlToJsonReturn = TURLObj | null;
 
 export const urlToJson = (url: string): TUrlToJsonReturn => {
   try {
     const urlObj = new URL(url);
     const params = urlObj.searchParams;
+
     const json: TUrlToJsonReturn = {
+      host: urlObj.host,
+      hostname: urlObj.hostname,
+      href: urlObj.href,
       origin: urlObj.origin,
       pathname: urlObj.pathname,
+      hash: urlObj.hash,
+      protocol: urlObj.protocol,
+      username: urlObj.username,
+      password: urlObj.password,
+      port: urlObj.port,
     };
     if (params.size > 0) {
       const searchParams: { [key: string]: string } = {};
       params.forEach((value, key) => {
         searchParams[key] = value;
       });
-      json.searchParams = searchParams;
+      json.params = searchParams;
     }
     return json;
   } catch {
